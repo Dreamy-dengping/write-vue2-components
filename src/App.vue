045efdc23fbb9da5cd6603cbfeaf306ae8b7c79a
dengping <dengping@kingchannels.com>
2022-10-14 16:01:27 +0800

    feat：完成Split组件

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,24 @@
 <template>
   <div class="app">
-    <div class="container">
-      <div class="aline">
-        <h2>普通效果</h2>
-        <Collapse v-model="activeName">
-          <Panel name="1">
-            史蒂夫·乔布斯
-            <p slot="content">
-              史蒂夫·乔布斯（Steve
-              Jobs），1955年2月24日生于美国加利福尼亚州旧金山，美国发明家、企业家、美国苹果公司联合创办人。
-            </p>
-          </Panel>
-          <Panel name="2">
-            斯蒂夫·盖瑞·沃兹尼亚克
-            <p slot="content">
-              斯蒂夫·盖瑞·沃兹尼亚克（Stephen Gary
-              Wozniak），美国电脑工程师，曾与史蒂夫·乔布斯合伙创立苹果电脑（今之苹果公司）。斯蒂夫·盖瑞·沃兹尼亚克曾就读于美国科罗拉多大学，后转学入美国著名高等学府加州大学伯克利分校（UC
-              Berkeley）并获得电机工程及计算机（EECS）本科学位（1987年）。
-            </p>
-          </Panel>
-          <Panel name="3">
-            乔纳森·伊夫
-            <p slot="content">
-              乔纳森·伊夫是一位工业设计师，现任Apple公司设计师兼资深副总裁，英国爵士。他曾参与设计了iPod，iMac，iPhone，iPad等众多苹果产品。除了乔布斯，他是对苹果那些著名的产品最有影响力的人。
-            </p>
-          </Panel>
-        </Collapse>
-        <div class="operate">
-          <button @click="handleClick">操作按钮</button>
+    <div class="aline">
+      <div class="item">
+        <h2>水平拖拽</h2>
+        <div class="demo-split">
+          <Split v-model="split1">
+            <div slot="left" class="demo-split-pane">Left Pane</div>
+            <div slot="right" class="demo-split-pane">Right Pane</div>
+          </Split>
         </div>
       </div>
-      <div class="aline">
-        <h2>手风琴效果</h2>
-        <Collapse accordion v-model="activeName1">
-          <Panel name="1">
-            史蒂夫·乔布斯
-            <p slot="content">
-              史蒂夫·乔布斯（Steve
-              Jobs），1955年2月24日生于美国加利福尼亚州旧金山，美国发明家、企业家、美国苹果公司联合创办人。
-            </p>
-          </Panel>
-          <Panel name="2">
-            斯蒂夫·盖瑞·沃兹尼亚克
-            <p slot="content">
-              斯蒂夫·盖瑞·沃兹尼亚克（Stephen Gary
-              Wozniak），美国电脑工程师，曾与史蒂夫·乔布斯合伙创立苹果电脑（今之苹果公司）。斯蒂夫·盖瑞·沃兹尼亚克曾就读于美国科罗拉多大学，后转学入美国著名高等学府加州大学伯克利分校（UC
-              Berkeley）并获得电机工程及计算机（EECS）本科学位（1987年）。
-            </p>
-          </Panel>
-          <Panel name="3">
-            乔纳森·伊夫
-            <p slot="content">
-              乔纳森·伊夫是一位工业设计师，现任Apple公司设计师兼资深副总裁，英国爵士。他曾参与设计了iPod，iMac，iPhone，iPad等众多苹果产品。除了乔布斯，他是对苹果那些著名的产品最有影响力的人。
-            </p>
-          </Panel>
-        </Collapse>
-        <div class="operate">
-          <button @click="handleClick1">操作按钮</button>
+      <div class="item">
+        <h2>垂直拖拽</h2>
+        <div class="demo-split">
+          <Split v-model="split2" mode="vertical">
+            <div slot="top" class="demo-split-pane">Top Pane</div>
+            <div slot="bottom" class="demo-split-pane">
+              Bottom Pane
+            </div>
+          </Split>
         </div>
       </div>
     </div>
@@ -64,22 +26,16 @@
 </template>
 
 <script>
-import { Panel, Collapse } from "./components/Collapse";
+import { Split } from "./components/Split/index";
 export default {
-  components: { Collapse, Panel },
+  components: {
+    Split,
+  },
   data() {
     return {
-      activeName: "1",
-      activeName1: "1",
+      split1: 0.1,
+      split2: 0.21,
     };
-  },
-  methods: {
-    handleClick() {
-      this.activeName = "3";
-    },
-    handleClick1() {
-      this.activeName1 = "3";
-    },
   },
 };
 </script>
@@ -101,7 +57,7 @@ export default {
     .operate {
       text-align: center;
     }
-    .aline{
+    .aline {
       width: 50%;
     }
     h2 {
@@ -116,5 +72,23 @@ export default {
     display: flex;
     justify-content: space-between;
   }
+}
+.aline {
+  display: flex;
+}
+.demo-split {
+  height: 200px;
+  border: 1px solid #dcdee2;
+  margin: 20px;
+}
+.item {
+  width: 50%;
+  h2 {
+    font-weight: bold;
+    text-align: center;
+  }
+}
+.demo-split-pane {
+  padding: 10px;
 }
 </style>
