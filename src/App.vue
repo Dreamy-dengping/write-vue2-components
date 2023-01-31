648e54f4d4e010feee6f4013c8790ed1bc27dd00
dengping <dengping@kingchannels.com>
2023-01-31 09:07:50 +0800

    feat：删除部分提交

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,22 @@
 <template>
   <div>
-    <div class="table" ref="table" style="margin-top:4000px">
-      <ul>
+    <div class="table" ref="table" style="margin-top: 4000px">
+      <!-- <ul>
         <li v-for="(item, index) in imgList"><img v-lazy="item" /></li>
-        <!-- <li v-for="(item, index) in imgList"><img v-lazy="item" /></li> -->
-        <!-- <li v-for="(item, index) in imgList"><img v-lazy="item" /></li> -->
-      </ul>
+      </ul> -->
+
+      
     </div>
   </div>
 </template>
 
 <script>
-import { Editor, Viewer } from "bytemd";
-import gfm from "@bytemd/plugin-gfm";
-const plugins = [
-  gfm(),
-  // Add more plugins here
-];
 export default {
-  components: {
-    Editor,
-  },
   data() {
-    return {
-      plugins,
-      imgList: [],
-    };
+    return {};
   },
-  mounted() {
-    setTimeout(() => {
-      this.imgList = [
-        require("@/assets/img/01.webp"),
-        require("@/assets/img/02.webp"),
-        require("@/assets/img/03.webp"),
-        require("@/assets/img/04.webp"),
-        require("@/assets/img/banner1.png"),
-        require("@/assets/img/banner2.png"),
-        require("@/assets/img/flower-banner.png"),
-      ];
-    }, 3000);
-  },
-  methods: {
-    handleChange(v) {
-      this.value = v;
-    },
-  },
+  mounted() {},
+  methods: {},
 };
 </script>
 
